3c26e9818867a006626e09f5b42569d29363a262
IgorKhnykin <igor.khnykin@mail.ru>
2025-03-12 13:27:35 +0300

    HW-20 completed

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/AuthApi.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/AuthApi.java
@@ -2,22 +2,9 @@ package guru.qa.niffler.api;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import retrofit2.Call;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.GET;
-import retrofit2.http.POST;
+import retrofit2.http.*;
 
 public interface AuthApi {
-
-    @FormUrlEncoded
-    @POST("oauth2/token")
-    Call<JsonNode> getToken(
-            @Field("code") String code,
-            @Field("redirect_uri") String redirectUri,
-            @Field("code_verifier") String codeVerifier,
-            @Field("grant_type") String grantType,
-            @Field("client_id") String clientId
-    );
 
     @FormUrlEncoded
     @POST("/register")
@@ -28,4 +15,29 @@ public interface AuthApi {
 
     @GET("/register")
     Call<Void> requestRegisterForm();
+    
+    @GET("/oauth2/authorize")
+    Call<Void> authorize(
+            @Query(value = "response_type") String responseType,
+            @Query(value = "client_id") String clientId,
+            @Query(value = "scope") String scope,
+            @Query(value = "redirect_uri") String redirectUri,
+            @Query(value = "code_challenge") String codeChallenge,
+            @Query(value = "code_challenge_method") String codeChallengeMethod);
+
+    @FormUrlEncoded
+    @POST("/login")
+    Call<Void> login(@Field("_csrf") String _csrf,
+                     @Field("username") String username,
+                     @Field("password") String password);
+
+    @FormUrlEncoded
+    @POST("oauth2/token")
+    Call<JsonNode> getToken(
+            @Field("code") String code,
+            @Field(value = "redirect_uri", encoded = true) String redirectUri,
+            @Field("code_verifier") String codeVerifier,
+            @Field("grant_type") String grantType,
+            @Field("client_id") String clientId
+    );
 }
